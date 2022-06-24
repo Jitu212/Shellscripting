0baf64b9a1cebab10515932e283757b405850c76
root <root@ip-172-31-1-169.ap-south-1.compute.internal>
2022-06-24 06:05:17 +0000

    echo and read commands

---
+++ scripting.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/bash
+echo "Hello World"
+
+echo $BASH
+echo $PWD
+echo $HOME
+
+name=Jitender 
+roll=212
+
+#variable name not start with number
+
+echo The name is $name
+echo roll no is $roll
+
+#read command take input from user it may be multiple
+
+echo "Enter the more name: "
+
+read name1
+read name2
+read name3
+
+echo "All given name are  : $name1, $name2 , $name3"
+
+read -p 'VIP name: ' name4 #print name in the same line
+echo "user name is $name4"
+
+read -sp 'Enter password: ' password
+echo "Password for all user is $password"
